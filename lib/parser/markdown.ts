018106c14016b2e49785adaf8e314c0d3ca4f624
Philipp Spiess <hello@philippspiess.com>
2025-06-13 14:43:40 +0200

    Add anchor links to markdown headings

--- lib/parser/markdown.ts
+++ lib/parser/markdown.ts
@@ -2,6 +2,8 @@ import { remark } from "remark"
 import rehypeShiki from "@shikijs/rehype"
 import rehypeStringify from "rehype-stringify"
 import remarkRehype from "remark-rehype"
+import rehypeSlug from "rehype-slug"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
 
 import matter from "gray-matter"
 import readingTime from "reading-time"
@@ -28,6 +30,18 @@ export async function parseMarkdown(markdown: string): Promise<{
 
   const processedContent = await remark()
     .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, {
+      behavior: 'prepend',
+      properties: {
+        className: ['header-anchor'],
+        ariaLabel: 'Link to this heading'
+      },
+      content: {
+        type: 'text',
+        value: '#'
+      }
+    })
     .use(rehypeShiki, {
       theme: "catppuccin-frappe",
       langs: [
