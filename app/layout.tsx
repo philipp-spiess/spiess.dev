88ff31ccab38da5661917f65a97f13442c1f3f0c
Philipp Spiess <hello@philippspiess.com>
2025-12-18 16:38:49 +0100

    Add `antialiased` class to root

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
