d8c75a88971eee8ab16ac53edcbd0ad1ff76cdf5
Philipp Spiess <hello@philippspiess.com>
2026-01-07 11:48:51 +0100

    Update bio: Somewhere between AI and UI

--- lib/Bio.tsx
+++ lib/Bio.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import Avatar from "./Avatar";
 
 export const description =
-  "Philipp Spiess is an engineer at Tailwind Labs with a focus on Web technologies. Previous roles include: Engineering at Sourcegraph, UI Engineer at Meta, curator of " +
-  "This Week in React, React DOM team member, and Team Lead at PSPDFKit.";
+  "Philipp Spiess – Somewhere between AI and UI. " +
+  "Prev: Tailwind Labs, Sourcegraph, Meta, React DOM team.";
 
 interface Props {
   direction?: "row" | "column";
@@ -36,7 +36,9 @@ export default function Bio({ direction }: Props) {
         </div>
 
         <p className="my-5">
-          Engineer at{" "}
+          Somewhere between AI and UI.
+          <br />
+          Prev:{" "}
           <Link
             href="https://tailwindcss.com"
             target="_blank"
@@ -44,30 +46,21 @@ export default function Bio({ direction }: Props) {
           >
             Tailwind Labs
           </Link>
-          .<br />
-          Prev: Engineer at{" "}
+          ,{" "}
           <Link
             href="https://sourcegraph.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             Sourcegraph
-          </Link>{" "}
-          and{" "}
+          </Link>
+          ,{" "}
           <Link
             href="https://facebook.com"
             target="_blank"
             rel="noopener noreferrer"
           >
             Meta
-          </Link>
-          , curator of{" "}
-          <Link
-            href="https://github.com/philipp-spiess/this-week-in-react"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            This Week in React
           </Link>
           ,{" "}
           <Link
@@ -77,15 +70,7 @@ export default function Bio({ direction }: Props) {
           >
             React DOM
           </Link>{" "}
-          team member, and Team Lead at{" "}
-          <Link
-            href="https://pspdfkit.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            PSPDFKit
-          </Link>
-          .
+          team
         </p>
         <ul className="m-0 flex list-none max-w-[300px]">
           <li className="m-[0.29rem] mt-0 first:ml-0 last:mr-0">
